ffda896f190aaee3b957483940d412f0380835df
Victor Ribeiro Boechat <victor2005contato@gmail.com>
2024-01-02 14:17:00 -0300

    refactor: make login page use composition api

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,4 @@
-<script lang="ts">
+<script setup lang="ts">
 interface ApiResponse {
   token: string;
   expiresIn: number;
@@ -7,56 +7,45 @@ interface ApiResponse {
 import { ref } from "vue";
 import { useRuntimeConfig } from "nuxt/app";
 
+const config = useRuntimeConfig();
+const router = useRouter();
+
+const formEmail = ref<string>("");
+const formPassword = ref<string>("");
 const isLoading = ref<boolean>(false);
 const hasError = ref<boolean>(false);
 
-export default {
-  data() {
-    return {
-      form: {
-        email: "",
-        password: "",
-      },
-      isLoading,
-      hasError,
-    };
-  },
-  setup() {
-    const config = useRuntimeConfig();
+const handleSubmit = async () => {
+  isLoading.value = true;
+  hasError.value = false;
 
-    return {
-      config,
-    };
-  },
-  methods: {
-    async handleSubmit() {
-      isLoading.value = true;
-      hasError.value = false;
+  const endpointRoute = `${config.public.BASE_URL}/auth/login`;
 
-      const endpointRoute = `${this.config.public.BASE_URL}/auth/login`;
+  const form = {
+    email: formEmail.value,
+    password: formPassword.value,
+  };
 
-      await $fetch(endpointRoute, {
-        method: "POST",
-        body: JSON.stringify(this.form),
-      })
-        .then((response) => {
-          const { token, expiresIn } = response as ApiResponse;
+  await $fetch(endpointRoute, {
+    method: "POST",
+    body: JSON.stringify(form),
+  })
+    .then((response) => {
+      const { token, expiresIn } = response as ApiResponse;
 
-          if (token) {
-            const expireInDate = getExpireInDate(expiresIn);
+      if (token) {
+        const expireInDate = getExpireInDate(expiresIn);
 
-            localStorage.setItem("token", token);
-            localStorage.setItem("expiresIn", expireInDate);
+        localStorage.setItem("token", token);
+        localStorage.setItem("expiresIn", expireInDate);
 
-            this.$router.push("/dashboard");
-          }
-          return;
-        })
-        .catch(() => (hasError.value = true));
+        router.push("/dashboard");
+      }
+      return;
+    })
+    .catch(() => (hasError.value = true));
 
-      isLoading.value = false;
-    },
-  },
+  isLoading.value = false;
 };
 </script>
 
@@ -71,13 +60,13 @@ export default {
       >
         <span class="text-5xl text-center text-zinc-100 mb-4">Autenticar</span>
         <input
-          v-model="form.email"
+          v-model="formEmail"
           type="email"
           placeholder="Seu e-mail"
           class="p-2 bg-zinc-600 rounded focus:outline-none focus:ring-4 focus:ring-indigo-500 placeholder:text-white text-white font-semibold"
         />
         <input
-          v-model="form.password"
+          v-model="formPassword"
           type="password"
           placeholder="Sua senha"
           class="p-2 bg-zinc-600 rounded focus:outline-none focus:ring-4 focus:ring-indigo-500 placeholder:text-white text-white font-semibold"
